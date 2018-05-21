5af28276cb2d68fcf359a297e3a38aeceff4e9ff
zhenhaoy <yezh0913@gmail.com>
2018-05-21 12:31:40 +0930

    11

---
+++ src/main/java/com/example/ec/domain/TourRatingPk.java
@@ -0,0 +1,44 @@
+package com.example.ec.domain;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Embeddable
+public class TourRatingPk implements Serializable {
+
+    @ManyToOne
+    private Tour tour;
+
+    @Column(insertable = false, updatable = false, nullable = false)
+    private Integer customerId;
+
+
+    /*
+    * @param tour          the tour
+    * @param customerId    the customer identifier
+    **/
+
+    public TourRatingPk(){}
+
+    public TourRatingPk(Tour tour, Integer customerId) {
+        this.tour = tour;
+        this.customerId = customerId;
+    }
+
+    public Tour getTour() {
+        return tour;
+    }
+
+    public void setTour(Tour tour) {
+        this.tour = tour;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+}
